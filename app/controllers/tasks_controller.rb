40677e2603c4b8a2385ab1e30958984ea852ae11
Emilia Zysk <emzysk.dev@gmail.com>
2023-07-01 14:01:07 +0900

    tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def find
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
